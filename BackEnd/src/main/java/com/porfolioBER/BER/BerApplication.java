6782722436ab12673066b2ad47d2fe779140078d
Brunao74 <brunoezerossi@gmail.com>
2022-05-24 16:01:28 -0300

    creacion back end

---
+++ BackEnd/src/main/java/com/porfolioBER/BER/BerApplication.java
@@ -0,0 +1,13 @@
+package com.porfolioBER.BER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/porfolioBER/BER/BerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolioBER.BER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
