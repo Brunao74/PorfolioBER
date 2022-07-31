09385926a0b89c55bc9cb03f072858195e5a5cd7
Brunao74 <brunoezerossi@gmail.com>
2022-07-30 21:03:09 -0300

    crearcuenta

--- BackEnd/src/main/java/com/porfolioBER/BER/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/porfolioBER/BER/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.porfolioBER.BER.Entity.Persona;
 import com.porfolioBER.BER.Interface.InterfacePersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,24 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class PersonaController {
     @Autowired InterfacePersonaService interfacepersonaService;
-    
     @GetMapping("personas/traer")
     public List<Persona> getPersona(){
         return interfacepersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         interfacepersonaService.savePersona(persona);
         return "creado";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/eliminar/(id)")
     public String deletePersona(@PathVariable Long id){
         interfacepersonaService.deletePersona(id);
         return "eliminado";
     }  
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/(id)")
     public Persona editPersona(@PathVariable Long id,
                             @RequestParam("nombre") String nuevoNombre,
@@ -44,7 +47,6 @@ public class PersonaController {
                             @RequestParam("img") String nuevoImg,
                             @RequestParam("titulo") String nuevoTitulo) {
         Persona persona = interfacepersonaService.findPersona(id);
-        
         persona.setNombre(nuevoNombre);
         persona.setApellido(nuevoApellido);
         persona.setImg(nuevoImg);
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
@@ -39,6 +40,8 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
     NgCircleProgressModule.forRoot({ }),
     HttpClientModule,
   ],
