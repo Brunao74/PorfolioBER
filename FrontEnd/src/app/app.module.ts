10de2e7b0bff221ff205022b99b2e4780bd395aa
Brunao74 <brunoezerossi@gmail.com>
2022-07-31 01:11:11 -0300

    registerfalta

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { LoginComponent } from './components/login/login.component';
     IngresoComponent,
     HomeComponent,
     LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
